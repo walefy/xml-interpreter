dc0d6f644ef20d1a87b1e4fb3b6c848094fc41cd
walefy <walefyd1@gmail.com>
2023-09-21 14:17:27 -0300

    feat: xmltoparse implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Header, HTTPException
+import xmltodict
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post('/xmltest')
+async def xml_test(upload_file: UploadFile = None, api_key: str = Header(None)):
+    if api_key != "123456":
+        raise HTTPException(status_code=401, detail="api-key header invalid!")
+    if upload_file is None:
+        raise HTTPException(status_code=400, detail="xml file not found!")
+    if upload_file.filename.endswith('.xml'):
+        doc = xmltodict.parse(upload_file.file.read())
+        return doc
+    else:
+        raise HTTPException(status_code=400, detail="xml file not found!")
