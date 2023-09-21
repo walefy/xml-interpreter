8e07119ed22d4b80bba9f6469e6bfe745186c39b
Herick2D <contato.herickmoreira@gmail.com>
2023-09-21 16:06:28 -0300

    feat: zip logic implementation

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI, UploadFile, Header, HTTPException
+from fastapi import FastAPI, Header, UploadFile, HTTPException
+from os import mkdir, listdir
+from shutil import rmtree
+import zipfile
 import xmltodict
 
 app = FastAPI()
+
+
+def compare_cnpj(cnpj: str, entry_file_name: str):
+    with open(f'{cnpj}/{entry_file_name}', 'r') as file:
+        response = xmltodict.parse(file.read())
+        list_key = ('nfeProc', 'NFe', 'infNFe', 'emit', 'CNPJ')
+        for key in list_key:
+            if isinstance(response, dict):
+                response = response.get(key, {})
+            else:
+                rmtree(cnpj)
+                raise HTTPException(status_code=400, detail="The xml file is not valid")
+
+        return response == cnpj
+        
+
+def compare_cnpj_in_all_files(folder_name: str, cnpj: str):
+    response = True
+    for file_name in listdir(folder_name):
+        if file_name.endswith('.xml'):
+            if not compare_cnpj(cnpj, file_name):
+                response = False
+                break
+        else:
+            rmtree(cnpj)
+            raise HTTPException(status_code=400, detail="all files must be xml!")
+    return response
 
 
 @app.get("/")
@@ -10,13 +40,20 @@ async def root():
 
 
 @app.post('/xmltest')
-async def xml_test(upload_file: UploadFile = None, api_key: str = Header(None)):
-    if api_key != "123456":
-        raise HTTPException(status_code=401, detail="api-key header invalid!")
+async def xml_test(upload_file: UploadFile = None, cnpj: str = Header(...)):
     if upload_file is None:
         raise HTTPException(status_code=400, detail="xml file not found!")
     if upload_file.filename.endswith('.xml'):
         doc = xmltodict.parse(upload_file.file.read())
         return doc
+    if upload_file.filename.endswith('.zip'):
+        mkdir(cnpj)
+        with zipfile.ZipFile(upload_file.file, 'r') as zip_ref:
+            zip_ref.extractall(cnpj)
+        if not compare_cnpj_in_all_files(cnpj, cnpj):
+            rmtree(cnpj)
+            raise HTTPException(status_code=400, detail="cnpj dont's match!")
+        rmtree(cnpj)
+        return {"detail": "All cnpj matchs!"}
     else:
         raise HTTPException(status_code=400, detail="xml file not found!")
