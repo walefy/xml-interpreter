141c63bd0bf66e3db4e71b68d905156988fe07b2
walefy <walefyd1@gmail.com>
2023-10-06 01:30:15 -0300

    test: send invoices with gap

--- tests/send_zip_file_test.py
+++ tests/send_zip_file_test.py
@@ -33,7 +33,12 @@ async def register_company():
 @pytest.mark.asyncio
 async def test_send_zip_with_invoices():
     async with AsyncClient(app=app, base_url='http://test') as async_client:
-        path_to_zip_file = path.join(path.dirname(__file__), 'data',  'noGapSeries.zip')
+        path_to_zip_file = path.join(
+            path.dirname(__file__),
+            'data',
+            'minimal_no_gap.zip'
+        )
+
         zip_file = open(path_to_zip_file, 'rb')
 
         files = {
@@ -49,4 +54,43 @@ async def test_send_zip_with_invoices():
         zip_file.close()
 
     assert response.json() == {'warnings': []}
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.asyncio
+async def test_send_zip_file_with_gap():
+    async with AsyncClient(app=app, base_url='http://test') as async_client:
+        path_to_zip_file = path.join(
+            path.dirname(__file__),
+            'data',
+            'minimal_with_gap.zip'
+        )
+
+        zip_file = open(path_to_zip_file, 'rb')
+
+        files = {
+            'upload_file': zip_file
+        }
+
+        headers = {
+            'Content-Type': 'multipart/form-data;boundary=boundary',
+            'cnpj': '40028176000176'
+        }
+
+        response = await async_client.post('/xmltest', files=files, headers=headers)
+        zip_file.close()
+
+    assert response.json() == {
+        'warnings': [
+            {
+                'message': 'There are missing invoices!',
+                'missing_invoices': [
+                    {'invoice_number': 3, 'serie': '3'},
+                    {'invoice_number': 4, 'serie': '3'},
+                    {'invoice_number': 66, 'serie': '2'},
+                    {'invoice_number': 67, 'serie': '2'},
+                ]
+            },
+        ],
+    }
     assert response.status_code == status.HTTP_201_CREATED
